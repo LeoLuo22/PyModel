1a3d98f1d6b5e55bd832b0e0fd0e2e5514755185
Leo <leoluo22@gmail.com>
2017-12-01 20:49:17 +0800

    Add test

--- testPyModel.py
+++ testPyModel.py
@@ -12,7 +12,7 @@ class TestModel(model.Model):
             Once you finished, use create_table() to save your model.
         """
         super(TestModel, self).__init__()
-        self.name = field.CharField(max_length=20)
+        self.name = field.CharField(max_length=20, unique=True)
         self.phone = field.CharField(max_length=20)
         self.sex = field.CharField(max_length=6)
         self.date = field.CharField(max_length=50)
@@ -27,17 +27,43 @@ def test_create_table():
     model.create_table()
 
 def test_save_data():
-    pass
+    """
+        In this case, it will generate a sql:
+        INSERT INTO TESTMODEL (NAME, PHONE, SEX, DATE) VALUES ( 'test', '18629508095', 'male', '2017/12/1' )
+    """
+    model = TestModel()
+    model.name = 'test'
+    model.phone = '18629508095'
+    model.sex = 'male'
+    model.date = '2017/12/1'
+
+    model.save()
 
 def test_update_table():
-    pass
+    """
+        In the example above, we did'n specify any field that is unique
+        What if we want to set NAME field is unique?
+    """
+    #This is to test update field's property
+    model = TestModel()
+    model.update_table()
+    #TODO Add field to an exist table
+    #TODO Delete field to an exist table
 
+def test_get_data():
+    """
+        Get data by the given condition
+    """
+    model = TestModel()
+    results = model.get_all(name='Leo')
+    print(results)
 def test_update_data():
     pass
 
 
 def main():
-    test_create_table()
+    #test_save_data()
+    test_get_data()
 
 if __name__ == '__main__':
     main()
