906a3c4ee23df34ee67d71afa6899732f5cafbe2
Leo <leoluo22@gmail.com>
2017-12-03 18:45:44 +0800

    Update CharField, IntegerField and FloatField

--- field.py
+++ field.py
@@ -1,62 +1,79 @@
+try:
+    from .utils import wrapper_str
+except SystemError:
+    from utils import wrapper_str
+
 class Field():
-    def __init__(self):
-        pass
+    def __init__(self, pk=False, required=False, default=None):
+        self.pk = pk#Primary key
+        self.required = required#Whether could be null
+        self.default = default#default value
 
 class CharField(Field):
-    def __init__(self, max_length=10, unique=False, required=False):
+    def __init__(self, max_length=10, pk=False, required=False, default=None):
+        super(CharField, self).__init__(pk, required, default)
         self.max_length = max_length
-        self.unique = unique
-        self.required = required
 
     def __str__(self):
         base = 'VARCHAR' + '(' + str(self.max_length) + ')'
         if self.required:
-            base += ' NOT NULL '
-        if self.unique:
+            base += ' NOT NULL'
+        if self.pk:
             base += ' PRIMARY KEY'
+        if self.default:
+            base += ' DEFAULT ' + wrapper_str(self.default)
 
         return base
 
 class IntegerField(Field):
-    def __init__(self, default=0, required=False):
-        self.default = default
-        self.required = required
+    def __init__(self, pk=False, required=False, default=None):
+        super(IntegerField, self).__init__(pk, required, default)
 
     def __str__(self):
         base = 'INT'
         if self.required:
-            base += ' NOT NULL '
-        base += ' DEFAULT ' + str(self.default)
+            base += ' NOT NULL'
+        if self.default:
+            base += ' DEFAULT ' + str(self.default)
 
         return base
 
 class FloatField(Field):
-    def __init__(self, default=0.0, required=False):
-        self.default = default
-        self.required = required
+    def __init__(self, pk=False, required=False, default=None):
+        super(FloatField, self).__init__(pk, required, default)
 
     def __str__(self):
         base = 'FLOAT'
         if self.required:
             base += ' NOT NULL '
-        base += ' DEFAULT ' + str(self.default)
+        if self.default:
+            base += ' DEFAULT ' + str(self.default)
 
         return base
 
 class BooleanField(Field):
-    def __init__(self, default=False):
-        self.default = default
+    def __init__(self, pk=False, required=False, default=None):
+        super(BooleanField, self).__init__(pk, required, default)
 
 class TextField(Field):
-    def __init__(self):
-        pass
+    def __init__(self, pk=False, required=False, default=None):
+        super(TextField, self).__init__(pk, required, default)
 
     def __str__(self):
         return 'TEXT'
 
+
+def test_charfield():
+    name = CharField(max_length=20, default='Leo', pk=True, required=True)
+    print(str(name))
+
+def test_integerfield():
+    number = IntegerField(required=True, default=1)
+    print(str(number))
+
 def main():
-    print(b'\xe2\x9c\x85\xe2\x9c\x85\xf0\x9f\x8c\xb9'.decode())
-    print('爱的太快'.encode())
-    print('✅✅🌹爱的太快'.encode('utf8').decode('utf8'))
+    test_charfield()
+    test_integerfield()
+
 if __name__ ==  '__main__':
     main()
